83ee5ec658addaf995ba8d1659c1875fb4c9e4ac
Przemek Wosinski <wosin7@gmail.com>
2018-08-09 20:36:09 +0200

    karuzela flickcity

---
+++ js/script.js
@@ -0,0 +1,34 @@
+'use strict';
+(function() {
+  var elem = document.querySelector('.main-carousel');
+    var flkty = new Flickity( elem, {
+      // options
+      cellAlign: 'left',
+      contain: true,
+      lazyLoad: true, 
+      pageDots: false, 
+      hash: true
+    });
+    //reset
+var flkty = new Flickity('.main-carousel');
+
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray( buttons );
+
+buttonGroup.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var index = buttons.indexOf( event.target );
+  flkty.select( index );
+});
+// pasek postepu
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
+})();
