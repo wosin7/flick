f07fbf67d2c4425b3ee23472c8eaac16b6ef0d05
Przemek Wosinski <wosin7@gmail.com>
2018-08-12 22:18:43 +0200

    Implementacja Mapy

--- js/script.js
+++ js/script.js
@@ -41,4 +41,26 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+window.initMap = function(){
+  var cord = [];
+  for (i=0; i<carusData.length; i++){
+    cord.push({lat: carusData[i].cords.lat, lng: carusData[i].cords.lng});
+  }
+  //var la = carusData[0].cords.lat;
+  //var lon = carusData[0].cords.lng;
+  console.log(typeof cord);
+  console.log(cord[1]);
+  
+  // The location of Uluru
+  var uluru = cord[0];
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+  var marker2 = new google.maps.Marker({position: cord[1], map: map});
+  var marker3 = new google.maps.Marker({position: cord[2], map: map});
+  var marker4 = new google.maps.Marker({position: cord[3], map: map});
+  var marker5 = new google.maps.Marker({position: cord[4], map: map});
+}
 })();
